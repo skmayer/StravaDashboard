8f3ce4d258c9088dab4a80c114f8c9f7cbdcabea
Sushil Mayer <skmayer@gmail.com>
2022-04-27 14:56:00 -0700

    remove waste

--- src/globals.js
+++ src/globals.js
@@ -1,7 +1,3 @@
 export const BASE_URL = "https://www.strava.com/api/v3";
 
-export const POSTER_PATH = "https://image.tmdb.org/t/p/original";
-
-export const API_KEY = process.env.REACT_APP_TMDB_KEY;
-
 export const TOKEN = process.env.REACT_APP_STRAVA_ACCESS_TOKEN;
